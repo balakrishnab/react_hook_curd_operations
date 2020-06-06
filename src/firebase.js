b4b710524466b3dd65a1bd9000c7caa689725fd9
Balakrishna <balakrishna.batta.ext@nokia.com>
2020-06-06 21:32:14 +0530

    removed the firestore config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: "AIzaSyC86E-Ju1zSteJR3PmpXuqLmqyS0L3Ejy0",
-	authDomain: "react-curd-todolist.firebaseapp.com",
-	databaseURL: "https://react-curd-todolist.firebaseio.com",
-	projectId: "react-curd-todolist",
-	storageBucket: "react-curd-todolist.appspot.com",
-	messagingSenderId: "541457011078",
-	appId: "1:541457011078:web:24d5244a61e1097b805c07"
+	// Add your firestore app config here
 };
 // Initialize Firebase
 var firebaseDB = firebase.initializeApp(firebaseConfig);
